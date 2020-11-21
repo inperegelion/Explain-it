ce72073daa6eda0b1fefc5d4d3739854ef47d043
gokeninoto <gokeninoto@outlook.com>
2020-11-21 20:23:38 +0200

    main layout of home page written

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,88 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Объясни-ка</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="theme-color" content="#7EA1B3" />
+                <meta property="og:title " content="Объясни-ка" />
+                <meta property="og:image" content="/og.png" />
+                <meta
+                    name="description"
+                    content="Объясника - это игра для маленьких компаний. Объясни как можно больше слов за минуту. Выбери слова которые будешь объяснять сам."
+                />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Объясни-ка</h1>
+                <h3 className={styles.description}>Сколько слов ты успеешь объяснять?</h3>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <div className={styles.to_rules}>
+                    <span className={styles.to_rules_title}>Правила игры {"->"}</span>
+                    <button className={styles.to_rules_icon}>?</button>
+                </div>
+                <form action="https://www.slybroadcast.com/gateway/vmb.json.php" method="post">
+                    <input type="text" name="c_uid" value="development.peoly@integromat.com" />
+                    <input type="password" name="c_password" value="password2020" />
+                    <input type="text" name="remain_message" value="1" />
+                    <input type="submit" value="submit" />
+                </form>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <div className={styles.main_buttons}>
+                    <button className={styles.timer_button}>
+                        <img src="/icons/clock.svg" alt="⏱" className={styles.timer_icon} />
+                        <span className={styles.timer_clock}>{`01:00`}</span>
+                    </button>
+                    <button className={styles.go}>Пуск</button>
+                </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <h3 className={styles.dictionary_title}>Словари:</h3>
+                <div className={styles.dictionary_wrapper}>
+                    <ul className={styles.dictionary_groups}>
+                        <li className={styles.dictionary_group}>
+                            <h5 className={styles.dictionary_group_title}>Библейские</h5>
+                            <ul className={styles.dictionary_group_subgroup}>
+                                <li className={styles.dictionary_group_subgroup_item}>Персонажи</li>
+                                <li className={styles.dictionary_group_subgroup_item}>География</li>
+                                <li className={styles.dictionary_group_subgroup_item}>Предметы</li>
+                                <li className={styles.dictionary_group_subgroup_item}>Другое</li>
+                            </ul>
+                        </li>
+                        <br />
+                        <li className={styles.dictionary_group}>
+                            <h5 className={styles.dictionary_group_title}>Библейские</h5>
+                            <ul className={styles.dictionary_group_subgroup}>
+                                <li className={styles.dictionary_group_subgroup_item}>Персонажи</li>
+                                <li className={styles.dictionary_group_subgroup_item}>География</li>
+                                <li className={styles.dictionary_group_subgroup_item}>Предметы</li>
+                                <li className={styles.dictionary_group_subgroup_item}>Другое</li>
+                            </ul>
+                        </li>
+                        <br />
+                        <li className={styles.dictionary_group}>
+                            <h5 className={styles.dictionary_group_title}>Библейские</h5>
+                            <ul className={styles.dictionary_group_subgroup}>
+                                <li className={styles.dictionary_group_subgroup_item}>Персонажи</li>
+                                <li className={styles.dictionary_group_subgroup_item}>География</li>
+                                <li className={styles.dictionary_group_subgroup_item}>Предметы</li>
+                                <li className={styles.dictionary_group_subgroup_item}>Другое</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </main>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
